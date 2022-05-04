673d2b5cf221510aea97c34c8756e27a174e2876
duru <kevin622@yonsei.ac.kr>
2022-05-04 18:05:58 +0900

    modified boj 12865

--- boj/12865_평범한배낭.py
+++ boj/12865_평범한배낭.py
@@ -1,16 +1,13 @@
 import sys
 
+input = sys.stdin.readline
+
 
 def main():
-    input = sys.stdin.readline
     N, K = map(int, input().split())
-    arr = [list(map(int, input().split())) for _ in range(N)]
-    arr.sort(key=lambda x: x[0])  # W, V
-    arr = [[0, 0]] + arr
     dp = [[0] * (K + 1) for _ in range(N + 1)]
-
     for i in range(1, N + 1):
-        w, v = arr[i]
+        w, v = map(int, input().split())
         for j in range(1, K + 1):
             dp[i][j] = dp[i - 1][j]
             if w <= j:
