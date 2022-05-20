5d3d6104d4cca2985ae8290203236d0b91ad76c5
duru <kevin622@yonsei.ac.kr>
2022-05-20 20:09:44 +0900

    이항계수3

--- boj/11401_이항계수3.py
+++ boj/11401_이항계수3.py
@@ -1,21 +1,32 @@
-# 아직 푸는 중
-N, K = map(int, input().split())
-mod = 1000007
+import sys
 
-# factorial
-factorial = list(range(N + 1))
-for i in range(3, N + 1):
-    factorial[i] = factorial[i] * factorial[i - 1] % mod
+input = sys.stdin.readline
 
 
 # power
-def power(a, b):
+def power(a, b, mod):
     if b == 1:
         return a
     elif b == 0:
         return 1
-    return (power(a, b // 2) ** 2) * power(a, b % 2) % mod
+    return (power(a, b // 2, mod) ** 2) * power(a, b % 2, mod) % mod
 
 
-answer = factorial[N] * power(factorial[K] * factorial[N - K], mod - 2) % mod
-print(answer)
+def main():
+    N, K = map(int, input().split())
+    mod = 1000000007
+
+    # factorial
+    factorial = list(range(N + 1))
+    factorial[0] = 1
+    for i in range(3, N + 1):
+        factorial[i] = factorial[i] * factorial[i - 1] % mod
+
+    A = factorial[N]
+    B = (factorial[N - K] * factorial[K]) % mod
+    answer = (A % mod) * (power(B, mod - 2, mod) % mod) % mod
+    sys.stdout.write(str(answer))
+
+
+if __name__ == '__main__':
+    main()
